dc956753f1014c332679f1de7240eddab153b07a
Robert Pickering <robert@strangelights.com>
2014-05-06 05:56:09 +0200

    rework peer to peer connections to allow the number of connections to be limited

--- src/app/CryptoCurrencyFun/PeerToPeer.fs
+++ src/app/CryptoCurrencyFun/PeerToPeer.fs
@@ -13,6 +13,7 @@ type internal SendConnections =
     | Remove of Socket
     | Broadcast of byte[]
     | SendMessage of IPAddress * byte[]
+    | ConnectFailed of IPAddress
 
 type MessageReceivedEventArgs(address: IPAddress, message: Message) =
     inherit EventArgs()
@@ -26,10 +27,11 @@ type internal ConnectionDetails =
         { Socket = s
           LastPing = DateTime.MinValue }
 
-
-type PeerToPeerConnectionManager(connDetails: NetworkDescription) =
+type PeerToPeerConnectionManager(connDetails: NetworkDescription, ?maxConnections) =
 
     let logger = LogManager.GetCurrentClassLogger()
+
+    let maxConnections = match maxConnections with Some x -> x | None -> Int32.MaxValue
 
     let messageProcessor =  new MessageProcessor(connDetails.MagicNumber)
 
@@ -60,55 +62,7 @@ type PeerToPeerConnectionManager(connDetails: NetworkDescription) =
             logger.Debug(sprintf "sending message to %O" ip)
             sendMessage socket buffer
 
-    // TODO any network connection  that throws an exception
-    let makeConnectionLoop (box: MailboxProcessor<_>) =
-        let rec connectionLoop (connections: Dictionary<IPAddress, ConnectionDetails>) =
-            async { try
-                        let! msg = box.Receive()
-                        let deadSockets = 
-                            connections 
-                            |> Seq.filter (fun x -> not x.Value.Socket.Connected)
-                            |> Seq.toList
-                        for deadSocket in deadSockets do
-                            connections.Remove(deadSocket.Key) |> ignore
-                        match msg with
-                        | Add socket ->
-                            let cd = ConnectionDetails.Create socket 
-                            let ip, _ = getRemoteAddress socket
-                            connections.Add(ip, cd)
-                            return! connectionLoop connections
-                        | Remove socket -> 
-                            // TODO this could fail, try removing by socket reference if it does
-                            let ip, _ = getRemoteAddress socket
-                            connections.Remove ip  |> ignore
-                            return! connectionLoop connections
-                        | Broadcast buffer ->
-                            let sockets =
-                                connections.Values |> Seq.map (fun x -> x.Socket) |> Seq.toArray
-                            broadcast sockets buffer
-                            return! connectionLoop connections
-                        | SendMessage (address, buffer) ->
-                            let succes, socket = 
-                                connections.TryGetValue(address) 
-                            match succes with
-                            | true -> sendMessage socket.Socket buffer
-                            | false -> logger.Error(sprintf "failed to send message to %O" address)
-                            return! connectionLoop connections
-                    with ex ->
-                        logger.Warn(sprintf "SAVED event loop from %O" ex)
-                        // TODO check if it's network excpetion and remove socket
-                        return! connectionLoop connections }
-        connectionLoop
-
-    let activeSendConnections =
-        MailboxProcessor.Start(fun box ->
-                                    let connectionLoop = makeConnectionLoop box
-                                    connectionLoop (new Dictionary<IPAddress, ConnectionDetails>()))
-
-    let sendMessageTo address messageBuffer =
-        activeSendConnections.Post(SendMessage(address, messageBuffer))
-
-    let startReadLoop socket =
+    let startReadLoop (activeConnections: MailboxProcessor<_>) socket =
 
         let rec processSegements header (segments: ResizeArray<byte[]>) =
 
@@ -155,28 +109,112 @@ type PeerToPeerConnectionManager(connDetails: NetworkDescription) =
                         return! loop header segments
                     with ex ->
                         logger.Error(printf "reading failed for %O exception was %O" socket.RemoteEndPoint ex)
-                        activeSendConnections.Post(Remove socket) }
+                        activeConnections.Post(Remove socket) }
         loop None (new ResizeArray<byte[]>())
 
-    let createAndStoreConnection address =
+    let createAndStoreConnection (activeConnections: MailboxProcessor<_>) address =
         async { try
                     logger.Info(sprintf "trying to connect to %O" address)
+                    // TODO can we lower the timeout here? failure to connect can take a long time
                     let! conn = AsyncSockets.OpenSendSocket address connDetails.Port
                     if conn.Connected then
-                        activeSendConnections.Post (Add conn)
+                        activeConnections.Post (Add conn)
                         let addressRemote, portRemote = getRemoteAddress conn
                         let addressLocal, portLocal = getLocalAddress conn
                         let version = 
                             Version.CreateMyVersion 
                                 addressRemote (portRemote |> uint16) addressLocal (portLocal |> uint16)
                         let buffer = messageProcessor.CreateBufferWithHeader version MessageNames.Version
-                        sendMessageTo address buffer
-                        startReadLoop conn |> Async.Start
+                        activeConnections.Post(SendMessage(address, buffer))
+                        startReadLoop activeConnections conn |> Async.Start
                         logger.Info(sprintf "connected to %O!" address)
                     else
                         logger.Error(sprintf "didn't connect to %O" address)
+                        activeConnections.Post(ConnectFailed address) 
                 with ex -> 
-                    logger.Error(sprintf "connection to address %O failed %O" address ex) }
+                    logger.Error(sprintf "connection to address %O failed %O" address ex)
+                    activeConnections.Post(ConnectFailed address) }
+
+
+
+    // TODO any network connection  that throws an exception
+    let makeConnectionLoop (box: MailboxProcessor<_>) =
+        let addressQueue = new Queue<IPAddress>(connDetails.GetIPAddresses())
+        let connections = new Dictionary<IPAddress, ConnectionDetails>()
+        let pendding = new HashSet<IPAddress>()
+
+        let connectToPeers() =
+            let remainingAddress = addressQueue.Count > 0
+            let currentConnections = connections.Count + pendding.Count
+            let moreConnectionsNeeded = currentConnections < maxConnections
+            if remainingAddress && moreConnectionsNeeded then
+                let count = min addressQueue.Count (maxConnections - currentConnections) 
+                logger.Debug(sprintf "count %i maxConnections %i" count maxConnections)
+                let newAddresses =
+                    [ for _ in 1 .. count do
+                        yield addressQueue.Dequeue() ]
+                for address in newAddresses do
+                    pendding.Add address |> ignore
+                newAddresses
+                |> Seq.map (createAndStoreConnection box)
+                |> Async.Parallel
+                |> Async.Ignore
+                |> Async.Start
+
+        let rec connectionLoop () =
+            async { try
+                        connectToPeers()
+                        // TODO 100ms time out is arbitary, may want to tune
+                        let! msg = box.TryReceive(100)
+                        let deadSockets = 
+                            connections 
+                            |> Seq.filter (fun x -> not x.Value.Socket.Connected)
+                            |> Seq.toList
+                        for deadSocket in deadSockets do
+                            connections.Remove(deadSocket.Key) |> ignore
+                        logger.Debug(sprintf "Active connnections %i" connections.Count)
+                        match msg with
+                        | Some(Add socket) ->
+                            let cd = ConnectionDetails.Create socket 
+                            let ip, _ = getRemoteAddress socket
+                            connections.Add(ip, cd)
+                            pendding.Remove(ip) |> ignore
+                            return! connectionLoop ()
+                        | Some(Remove socket) -> 
+                            // TODO this could fail, try removing by socket reference if it does
+                            let ip, _ = getRemoteAddress socket
+                            connections.Remove ip  |> ignore
+                            return! connectionLoop ()
+                        | Some(Broadcast buffer) ->
+                            let sockets =
+                                connections.Values |> Seq.map (fun x -> x.Socket) |> Seq.toArray
+                            broadcast sockets buffer
+                            return! connectionLoop ()
+                        | Some(SendMessage (address, buffer)) ->
+                            let succes, socket = 
+                                connections.TryGetValue(address) 
+                            match succes with
+                            | true -> sendMessage socket.Socket buffer
+                            | false -> logger.Error(sprintf "failed to send message to %O" address)
+                            return! connectionLoop ()
+                        | Some(ConnectFailed address) ->
+                            pendding.Remove address |> ignore
+                            return! connectionLoop ()
+                        | None ->
+                            return! connectionLoop ()
+                    with ex ->
+                        logger.Warn(sprintf "SAVED event loop from %O" ex)
+                        // TODO check if it's network excpetion and remove socket
+                        return! connectionLoop () }
+        connectionLoop
+
+    let activeConnections =
+        new MailboxProcessor<_>(fun box ->
+                                    let connectionLoop = makeConnectionLoop box
+                                    connectionLoop ())
+
+    let sendMessageTo address messageBuffer =
+        activeConnections.Post(SendMessage(address, messageBuffer))
 
     let startAcceptLoop() =
         // why? because msdn says so ... http://msdn.microsoft.com/en-us/library/dz10xcwh(v=vs.110).aspx
@@ -191,18 +229,11 @@ type PeerToPeerConnectionManager(connDetails: NetworkDescription) =
                         let! conn = AsyncSockets.Accept socket
                         let address = getRemoteAddress conn
                         logger.Info(sprintf "got connection from %O" address)
-                        startReadLoop conn |> Async.Start
+                        startReadLoop activeConnections conn |> Async.Start
                         return! loop()
                     with ex -> 
                         logger.Error(sprintf "readloop failed %O" ex) }
         loop()
-
-    let openConnections() =
-        connDetails.GetIPAddresses()
-        |> Seq.map createAndStoreConnection
-        |> Async.Parallel
-        |> Async.Ignore
-        |> Async.Start
 
     let handleMessage (address, message) =
         match message with
@@ -237,14 +268,13 @@ type PeerToPeerConnectionManager(connDetails: NetworkDescription) =
 
     do messageProcessor.MessageReceived |> Event.add handleMessage
 
-    // TOOD need a way to specify whether we should limit connections or try to maximize them
     member x.Connect() =
         startAcceptLoop() |> Async.Start
-        openConnections()
+        activeConnections.Start()
 
     member x.Broadcast message =
         let buffer = messageProcessor.CreateBufferWithHeaderFromMessage message
-        activeSendConnections.Post(Broadcast buffer)
+        activeConnections.Post(Broadcast buffer)
 
     member x.SendTo address message =
         let buffer = messageProcessor.CreateBufferWithHeaderFromMessage message
